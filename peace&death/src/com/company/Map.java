eb37c657679db1cab2e3bc1a0f3ff608de8f540f
DoSSi4 <57913839+DoSSi4@users.noreply.github.com>
2020-06-12 13:41:31 +0600

    Update Map.java

--- peace&death/src/com/company/Map.java
+++ peace&death/src/com/company/Map.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
-
+//Creating Map on JFX Panel
 public class Map extends Pane {
-    private static final int UNIT = 20;
-    private int width;
+    private static final int UNIT = 20; // declare 1 Unit on Map as 20 px;
+    private int width; 
     private int height;
-    private static int[][] map;
-
+    private static int[][] map; //2D-array to create map
     private Position startPos;
-
+    //Constructing Map with choosing level
     public Map(String level) {
         Scanner scan = new Scanner(System.in);
         try {
@@ -27,24 +26,22 @@ public class Map extends Pane {
         for(int i=0; i<height; i++) {
             for(int j=0; j<width; j++) {
                 map[i][j] = scan.nextInt();
-                //System.out.print(map[i][j] + " ");
-                if(map[i][j] == 2) {
+                if(map[i][j] == 2) { // 2 in map means startPos of Head
                     startPos = new Position(j, i);
                 }
-                if(map[i][j] == 1) {
+                if(map[i][j] == 1) { // 1 in map means black border
                     Rectangle rec = new Rectangle(j*UNIT, i*UNIT, UNIT, UNIT);
                     getChildren().add(rec);
                 }
-                if(map[i][j] == 3) {
+                if(map[i][j] == 3) { // 3 just avoiding 
                     map[i][j] = 1;
                 }
             }
-            //System.out.println();
         }
 
         scan.close();
     }
-
+    //Getters for map
     public int getMapWidth(){
         return width;
     }
@@ -58,11 +55,10 @@ public class Map extends Pane {
         return startPos;
     }
 
-
+    //Check method for creating snake
     public static boolean validate(int x, int y) {
-        if(map[y][x] != 1)
-            return true;
-        return false;
+        if(map[y][x] != 1) {return true;}
+        else {return false;}
     }
 
 }
