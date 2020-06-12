923f5d9485bb749b244f3a5774ef7a99e9fb53a0
Venturos0 <57934166+Venturos0@users.noreply.github.com>
2020-06-12 14:59:34 +0600

    Add files via upload

---
+++ peace&death/src/com/company/Player.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface Player {
+    public void moveRight();
+    public void moveDown();
+    public void moveLeft();
+    public void moveUp();
+    public void update();
+}
